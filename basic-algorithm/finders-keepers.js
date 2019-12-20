377c64abf882e861e03e1533111ac23b8f3bc8b8
Jenny Nguy <j.nguy809@gmail.com>
2019-12-19 23:51:38 -0500

    function returns first element in array that passes given function

---
+++ basic-algorithm/finders-keepers.js
@@ -0,0 +1,10 @@
+function findElement(arr, func) {
+  for (let num of arr) {
+    if (func(num)) {
+      return num
+    }
+  }
+  return undefined;
+}
+
+console.log(findElement([1, 2, 3, 4], num => num % 2 === 0));
