e9801d951a85f2309bac091d75688f2667d7225e
Jenny Nguy <j.nguy809@gmail.com>
2019-12-20 13:20:00 -0500

    function converts celsius to fahrenheit

---
+++ basic-algorithm/ctof.js
@@ -0,0 +1,7 @@
+//The algorithm to convert from Celsius to Fahrenheit is the temperature in Celsius times 9/5, plus 32.
+
+function convertToF(celsius) {
+  return ((celsius * 9 / 5) + 32)
+}
+
+convertToF(30);
