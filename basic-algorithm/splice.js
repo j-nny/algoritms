e7e7a845b149469615e48e681894959713b122a2
Jenny Nguy <j.nguy809@gmail.com>
2019-12-20 12:40:14 -0500

    insert one array into the other

---
+++ basic-algorithm/splice.js
@@ -0,0 +1,8 @@
+// Use the array methods slice and splice to copy each element of the first array into the second array, in order, at index n.
+
+function frankenSplice(arr1, arr2, n) {
+  arr2.splice(n, 0, ...arr1)
+  return arr2;
+}
+
+console.log(frankenSplice([1, 2, 3], [4, 5, 6], 1));
